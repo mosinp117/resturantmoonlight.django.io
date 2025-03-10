5c4d54835ddb6f80843dd5cf4ca90bf7d4b0c669
mosinp117 <mosinp889@gmail.com>
2025-03-10 13:06:54 +0530

    Initial commit for Restaurant Project

--- Resturantss/Resturant_Project/Resturant_Project/settings.py
+++ Resturantss/Resturant_Project/Resturant_Project/settings.py
@@ -26,7 +26,13 @@ SECRET_KEY = 'django-insecure-t_!n%3ph(-bpkd2+z2!zujx+6r_x%&3%allmra#8#x0v*iogzf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ".vercel.app,127.0.0.1").split(",")
+
+
+
 
 
 # Application definition
@@ -122,6 +128,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR / "static")]
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles_build", "static")
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
